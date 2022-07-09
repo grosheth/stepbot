699ddc3b5524ee9d7398cf659f597aaaa0209633
Salledelavage <alextrington@gmail.com>
2022-07-09 14:50:12 -0400

    automatic push

--- discord_bot/wallet.py
+++ discord_bot/wallet.py
@@ -2,22 +2,22 @@ from pymongo import MongoClient
 
 class Wallet(Document):
 
-    def create_wallet(user, money)
-        for member in ctx.guild.members:
-            print(member.id)
-            cashflow = Wallet.wallet(member.id, 10000)
-            print(cashflow)
+    # def create_wallet(user, money)
+    #     for member in ctx.guild.members:
+    #         print(member.id)
+    #         cashflow = Wallet.wallet(member.id, 10000)
+    #         print(cashflow)
 
-    def wallet(user, money):
+    # def wallet(user, money):
 
-        connect = MongoClient(CONN_STRING)
-        db = connect.discord
-        collection = db.wallet
+    #     connect = MongoClient(CONN_STRING)
+    #     db = connect.discord
+    #     collection = db.wallet
     
-        wallets = {
-            str(user): money
-        }
+    #     wallets = {
+    #         str(user): money
+    #     }
         
-        collection.insert_one(cashflow)
+    #     collection.insert_one(cashflow)
 
-        return wallets
+    #     return wallets
