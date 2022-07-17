feb45f30614c74b64a93f84e73d897a31a1df962
salledelavage <alextrington@gmail.com>
2022-07-17 16:36:53 -0400

    automatic push

--- discord_bot/cogs/spotify.py
+++ discord_bot/cogs/spotify.py
@@ -40,6 +40,7 @@ class Music(commands.Cog):
             await ctx.voice_client.move_to(voice_channel)
 
         ctx.voice_client.stop()
+        
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format':"bestaudio"}
         vc = ctx.voice_client
