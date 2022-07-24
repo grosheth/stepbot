b6a3d333ac35eb8eedc02bf4fd48a1d1f85bbfcf
salledevage <alextrington@gmail.com>
2022-07-24 14:08:09 -0400

    commit automatique

--- discord_bot/cogs/activities.py
+++ discord_bot/cogs/activities.py
@@ -41,11 +41,7 @@ class Activities(commands.Cog):
             print(f'Bot {member.name} joined {after.channel.name}')
 
         voice_channel = member.voice.channel
-
-        if member.voice_client is None:
-            await voice_channel.connect()
-
-        member.voice_client.stop()
+        await voice_channel.connect()
 
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format':"bestaudio"}
