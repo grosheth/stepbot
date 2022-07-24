8a74d9c7de461feb435c8a9473d4cd904356fca9
salledevage <alextrington@gmail.com>
2022-07-24 14:40:13 -0400

    commit automatique

--- discord_bot/cogs/activities.py
+++ discord_bot/cogs/activities.py
@@ -49,7 +49,7 @@ class Activities(commands.Cog):
 
             FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
             YDL_OPTIONS = {'format':"bestaudio"}
-            vc = member.voice_client
+            vc = member.voice.client
 
             with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
                 info = ydl.extract_info(url, download=False)
