3b9e9e4ed558b96fc655aa611eb66da8e2727954
salledevage <alextrington@gmail.com>
2022-07-24 15:51:20 -0400

    commit automatique

--- discord_bot/wallet.py
+++ discord_bot/wallet.py
@@ -8,17 +8,17 @@ class Wallet():
     #         cashflow = Wallet.wallet(member.id, 10000)
     #         print(cashflow)
 
-    def reset_wallet():
-        connect = MongoClient(CONN_STRING)
-        db = connect.discord
-        collection = db.wallet
-        for member in ctx.guild.members:
-            print(member.id)
-            money = 25000
-            collection.update_one({
-            '_id': 	member.id
-            },{
-            '$set': {
-                'Nanane': money
-            }
-            }, upsert=True)
+    # def reset_wallet():
+    #     connect = MongoClient(CONN_STRING)
+    #     db = connect.discord
+    #     collection = db.wallet
+    #     for member in ctx.guild.members:
+    #         print(member.id)
+    #         money = 25000
+    #         collection.update_one({
+    #         '_id': 	member.id
+    #         },{
+    #         '$set': {
+    #             'Nanane': money
+    #         }
+    #         }, upsert=True)
