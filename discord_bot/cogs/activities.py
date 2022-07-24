8afd6c6acba5e9d62e7c861c538ee0184d71b561
salledevage <alextrington@gmail.com>
2022-07-24 14:32:44 -0400

    commit automatique

--- discord_bot/cogs/activities.py
+++ discord_bot/cogs/activities.py
@@ -40,22 +40,22 @@ class Activities(commands.Cog):
         if member.id == int(ALESS):
             url = "https://www.youtube.com/watch?v=U06jlgpMtQs"
 
-        if not before.channel:
-            print(f'Bot {member.name} joined {after.channel.name}')
+            if not before.channel:
+                print(f'Bot {member.name} joined {after.channel.name}')
 
-        voice_channel = member.voice.channel
-        await voice_channel.connect()
+            voice_channel = member.voice.channel
+            await voice_channel.connect()
 
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        YDL_OPTIONS = {'format':"bestaudio"}
-        # vc = member.voice_client
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            YDL_OPTIONS = {'format':"bestaudio"}
+            vc = member.voice_client
 
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2,
-            **FFMPEG_OPTIONS)
-            voice_channel.play(source)
+            with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(url, download=False)
+                url2 = info['formats'][0]['url']
+                source = await discord.FFmpegOpusAudio.from_probe(url2,
+                **FFMPEG_OPTIONS)
+                vc.play(source)
 
 
 def setup(bot):
