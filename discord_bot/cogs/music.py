75c61c00eaa2dba082518f90631fb6d9174717a1
salledevage <alextrington@gmail.com>
2022-07-30 09:39:27 -0400

    commit automatique

--- discord_bot/cogs/music.py
+++ discord_bot/cogs/music.py
@@ -47,7 +47,6 @@ class Music(commands.Cog):
 
 
         for x in url_list:
-            url_list.remove(x)
             print(url_list, x)
             with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
                 info = ydl.extract_info(x, download=False)
@@ -55,6 +54,7 @@ class Music(commands.Cog):
                 source = await discord.FFmpegOpusAudio.from_probe(url2,
                 **FFMPEG_OPTIONS)
                 vc.play(source)
+                url_list.remove(x)
                 ctx.voice_client.stop()
 
 
