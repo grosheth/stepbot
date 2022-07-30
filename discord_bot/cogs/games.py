28bb70c93323f743c7f1354c001d3f2263ac22d1
salledevage <alextrington@gmail.com>
2022-07-30 12:28:53 -0400

    commit automatique

--- discord_bot/cogs/games.py
+++ discord_bot/cogs/games.py
@@ -80,35 +80,6 @@ class Games(commands.Cog):
         await ctx.send(f"Parsonne est mort")
         return
 
-    @commands.command(brief="!disconnect")
-    async def disconnect(self, ctx):
-        await ctx.voice_client.disconnect()
-
-
-    @commands.command(brief="!play <url> pour jouser de la music")
-    async def play(self, ctx, url):
-
-        if ctx.author.voice is None:
-            await ctx.send("T po dans l'channel, Tu decide po.")
-        voice_channel = ctx.author.voice.channel
-
-        if ctx.voice_client is None:
-            await voice_channel.connect()
-        else:
-            await ctx.voice_client.move_to(voice_channel)
-        
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-        YDL_OPTIONS = {'format':"bestaudio"}
-        vc = ctx.voice_client
-
-
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            url2 = info['formats'][0]['url']
-            source = await discord.FFmpegOpusAudio.from_probe(url2,
-            **FFMPEG_OPTIONS)
-            await vc.play(source)
-
 
 def setup(bot):
     bot.add_cog(Games(bot))
