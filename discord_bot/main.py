ee781ce0e69feb1b3e098acecb2d62fc64380814
grosheth <alextrington@gmail.com>
2022-08-31 19:22:11 -0400

    push automatique

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -8,8 +8,13 @@ dotenv.load_dotenv()
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 # load files in cogs folder
-for filename in os.listdir("./cogs"):
-	if filename.endswith(".py") and filename != "__init__.py":
-		bot.load_extension(f'cogs.{filename[:-3]}')
+try:
+	for filename in os.listdir("./cogs"):
+		if filename.endswith(".py") and filename != "__init__.py":
+			bot.load_extension(f'cogs.{filename[:-3]}')
+except:
+        for filename in os.listdir("./discord_bot/cogs"):
+                if filename.endswith(".py") and filename != "__init__.py":
+                        bot.load_extension(f'cogs.{filename[:-3]}')
 
 bot.run(TOKEN)
