3cdf1853a39033f72227607a05a84d3b02714c58
salledelavage <salledelavager@gmail.com>
2024-07-28 16:42:05 -0400

    dots

--- dockerfile.sh
+++ dockerfile.sh
@@ -7,13 +7,13 @@ if [[ $1 == 'a' || $1 == 'all' ]]; then
 fi
 
 if [[ $1 == 'os' ]]; then
-	docker build -t stepbot-os -f dockerfile-os
+	docker build -t stepbot-os -f dockerfile-os .
 fi
 
 if [[ $1 == 'p' ]]; then
-	docker build -t stepbot-packages -f dockerfile-packages
+	docker build -t stepbot-packages -f dockerfile-packages .
 fi
 
 if [[ $1 == 'pip' || $1 == 'python' ]]; then
-	docker build -t stepbot -f dockerfile-requirements
+	docker build -t stepbot -f dockerfile-requirements .
 fi
