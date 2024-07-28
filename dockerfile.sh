3337fd3eb6bcb99e9f34843c1226c92b1c2679b6
salledelavage <salledelavager@gmail.com>
2024-07-28 16:40:28 -0400

    two scripts

--- dockerfile.sh
+++ dockerfile.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/bash
 
 if [[ $1 == 'a' || $1 == 'all' ]]; then
-	podman build -t stepbot-os -f dockerfile-os
-	podman build -t stepbot-packages -f dockerfile-packages
-	podman build -t stepbot -f dockerfile-requirements
+	docker build -t stepbot-os -f dockerfile-os
+	docker build -t stepbot-packages -f dockerfile-packages
+	docker build -t stepbot -f dockerfile-requirements
 fi
 
 if [[ $1 == 'os' ]]; then
-	podman build -t stepbot-os -f dockerfile-os
+	docker build -t stepbot-os -f dockerfile-os
 fi
 
 if [[ $1 == 'p' ]]; then
-	podman build -t stepbot-packages -f dockerfile-packages
+	docker build -t stepbot-packages -f dockerfile-packages
 fi
 
 if [[ $1 == 'pip' || $1 == 'python' ]]; then
-	podman build -t stepbot -f dockerfile-requirements
+	docker build -t stepbot -f dockerfile-requirements
 fi
